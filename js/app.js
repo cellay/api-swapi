49b5936a1d936770187e1fffae45900e76b33fce
cellay <camposellay@gmail.com>
2018-02-16 06:46:30 -0500

    Feedback

--- js/app.js
+++ js/app.js
@@ -3,10 +3,12 @@ $(document).ready(function() {
   let URI = `https://swapi.co/api/people`;
 
   let getSWCharacters = (data) => {
+    let count = 1;
     let sWC = data.results;
     $.each(sWC, (i, char) => {
-      $('.container-box').append(`<div class="card col-4"><p>${char.name}</p> <a href="#" class='seeMore' data-toggle="modal" data-target="#modalChrtr" data-link= "${char.url}">See more</a></div>       
-            `);
+      let card = `<div class="card card-body"><p>${char.name}</p> <img class="card-img-top" src="https://raw.githubusercontent.com/tbone849/star-wars-guide/master/build/assets/img/characters/${count}.jpg" alt=""> <a href="#" class='seeMore card-link' data-toggle="modal" data-target="#modalChrtr" data-link= "${char.url}">See more</a></div>`;
+      $('.container-box').append(card);
+      count++;
     });
     $('.seeMore').click(swInfo);
   };
@@ -22,13 +24,12 @@ $(document).ready(function() {
             <p><b>Mass: </b>${data.mass}lb</p>
             <p><b>Hair color: </b>${data.hair_color}</p>
             <p><b>Skin color: </b>${data.skin_color}</p>`);
-      console.log(data)
-    }
+    };
 
     $.ajax({
       url: url,
       success: getSWInfo
-    })
+    });
   };
 
   $.ajax({
@@ -43,14 +44,16 @@ $(document).ready(function() {
 
     let getSWCharacter = (data) => {
       let sWC = data.results[0];
-      $('.container-box').append(`<div class='col-4 card'><p>${sWC.name}</p> <a href="#" class='seeMore' data-toggle="modal" data-target="#modalChrtr" data-link= "${sWC.url}">See more</a></div>       
+      console.log(data);
+      let count = sWC.url.slice(28, -1);
+      $('.container-box').append(`<div class='card card-body'><p>${sWC.name}</p> <img class="card-img-top" src="https://raw.githubusercontent.com/tbone849/star-wars-guide/master/build/assets/img/characters/${count}.jpg" alt=""> <a href="#" class='seeMore card-link' data-toggle="modal" data-target="#modalChrtr" data-link= "${sWC.url}">See more</a></div>       
             `);
       $('.seeMore').click(swInfo);
-    }
+    };
 
     $.ajax({
       url: URI + '/?search=' + $searchSWC,
       success: getSWCharacter
     });
-  });
+  }); 
 });
